d2bb811e1e9d95c1146af3ebd65d8763f380ee2d
Dima Voit <dmitriy.voyt@gmail.com>
2022-01-31 00:20:12 +0200

    del comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,5 @@
-// $(document).ready(function() {
-//   $('.header__burger').click(function(event) {
-//     $('.nav', '.header__burger').toggleClass('active');
-//   });
-// });
-
 $(document).ready(function() {
-  $('.header__burger').on(click, function(event) {
-    $('.nav', '.header__burger').toggleClass('active');
+  $('.header__burger').on('click', function(event) {
+    $('.nav, .header__burger').toggleClass('active');
   });
 });

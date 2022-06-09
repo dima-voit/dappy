35f2a3a2aa9a23d808df1cdb54424fd13de6e661
Dima Voit <dmitriy.voyt@gmail.com>
2022-06-09 20:51:41 +0300

    add modal window

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,32 @@
+// Burger menu
 $(document).ready(function() {
   $('.header__burger').on('click', function(event) {
     $('.nav, .header__burger').toggleClass('active');
   });
+});
+
+// Modal
+$('[data-modal]').on('click', function(event) {
+  event.preventDefault();
+  let modal = $(this).data('modal');
+  
+  $('body').addClass('no-scroll');
+  $(modal).addClass('show');
+});
+
+$('[data-modal-close]').on('click', function(event) {
+  event.preventDefault();
+  let modal = $(this).parents('.modal');
+  
+  $('body').removeClass('no-scroll');
+  $(modal).removeClass('show');
+});
+
+$('.modal').on('click', function() {
+  $('body').removeClass('no-scroll');
+  $(this).removeClass('show');
+});
+
+$('.modal__content').on('click', function(event) {
+  event.stopPropagation();
 });
